9f1e396c62304e3d88c0b760a1896ac25c609e49
Shruthi P <shruthi.2209@gmail.com>
2020-07-24 20:15:49 -0400

    missing toString method

--- src/main/java/com/shruthi/customer/data/entities/Customer.java
+++ src/main/java/com/shruthi/customer/data/entities/Customer.java
@@ -34,4 +34,9 @@ public class Customer {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
 }
